bffc631da1708a53c5b886f0e03e8c62281e4344
Junhong Zhu <a1712762@student.adelaide.edu.au>
2022-08-04 11:29:14 +0800

    - 清理无用演示代码

---
+++ src/pages/home/Home.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <div>{{ msg }}</div>
+    <router-link to="/list">列表页</router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+  data () {
+    return {
+      msg: 'home'
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
